06ae1c3117a1d0e9b7052a1520c4a8327188bdcd
mao mao <WMC130302@163.com>
2024-07-04 17:48:34 +0800

    solve problem on ishappy.py

--- leecodeStudy/pyplot-founction.py
+++ leecodeStudy/pyplot-founction.py
@@ -3,11 +3,10 @@ import numpy as np
 
 xpoints = np.array([])
 ypoints = np.array([])
-for i in range(-10000, 10000):
+for i in range(-100000000, 100000000):
     xpoints = np.append(xpoints, i)
     ypoints = np.append(ypoints, (2 * i - 1) * (3 * i - 2) * (4 * i - 3))
 
-print(xpoints, ypoints)
 
 plt.plot(xpoints, ypoints)
 plt.show()
