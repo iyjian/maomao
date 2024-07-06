7c7d8344afd525fd8b92b7c93723d07ffd2e9f1f
mao mao <WMC130302@163.com>
2024-07-06 20:37:10 +0800

    add dev container

--- leecodeStudy/combination.cpp
+++ leecodeStudy/combination.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
 
-
-int combination(unsigned int n, unsigned int m){
+// int combination(unsigned int n, unsigned int m){
     
+// }
+
+int main() {
+    std::cout << "hello" << std::endl;
 }
-std::cout << "Factorial of " << number << " is " << 
+// std::cout << "Factorial of " << number << " is " << 
