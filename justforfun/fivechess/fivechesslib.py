0bf16a0e0a4a732240b9b9b2db81373aec678a54
w.c <52445078@qq.com>
2024-07-26 05:58:43 +0000

    add check_win_left_slash function in fivechesslib

--- justforfun/fivechess/fivechesslib.py
+++ justforfun/fivechess/fivechesslib.py
@@ -1,3 +1,6 @@
+from operator import length_hint
+
+
 BLACK_WIN = 1
 WHITE_WIN = 2
 NOBODY_WIN = 0
@@ -5,9 +8,7 @@ BLACK_CHESS = 1
 WHITE_CHESS = 2
 NOTHING = 0
 
-# [BLACK_CHESS, BLACK_CHESS, BLACK_CHESS, BLACK_CHESS, BLACK_CHESS, NOTHING, WHITE_CHESS, BLACK_CHESS, BLACK_CHESS, BLACK_CHESS]
 def __check_win_line__(line):
-  # this is a private function, you don't have to use it 
   black_chess_continue_num = 0
   white_chess_continue_num = 0
   
@@ -38,7 +39,6 @@ def __check_win_line__(line):
 
 
 def __check_win_lying_flat__(chess_board):
-  # this is a private function, you don't have to use it 
   for i in chess_board:
     if __check_win_line__(i) == NOBODY_WIN:
       continue
@@ -49,8 +49,7 @@ def __check_win_lying_flat__(chess_board):
   return NOBODY_WIN
 
 def __check_win_vertically__(chess_board):
-  # this is a private function, you don't have to use it 
-  chess_board_length = len(chess_board[1])
+  chess_board_length = len(chess_board)
   vertical_chess_board = []
   for i in range(chess_board_length):
     vertical_chess_board.append([])
@@ -59,6 +58,28 @@ def __check_win_vertically__(chess_board):
       vertical_chess_board[k].append(j[k])
   return __check_win_lying_flat__(vertical_chess_board)
 
+def __get_slash_coordinate_arr__(chess_board, slash_number):
+  length_of_chess_board = len(chess_board)
+  slash_array = []
+  if slash_number <= length_of_chess_board - 1:
+    for i in range(slash_number + 1):
+      slash_array.append(chess_board[i][slash_number - i])
+  elif slash_number > length_of_chess_board - 1:
+    for i in range((slash_number - length_of_chess_board + 1), (- slash_number + 2 * length_of_chess_board + 2)):
+      slash_array.append(chess_board[i][slash_number - i])
+  else:
+    print('error : slash_number is not right')
+    raise
+  return slash_array
+
+def __get_slashes__(chess_board):
+  length_of_chess_board = len(chess_board)
+  slash_chess_board = []
+  for slash_number in range(length_of_chess_board):
+    slash_chess_board.append(__get_slash_coordinate_arr__(chess_board, slash_number))
+  return slash_chess_board
+
+
 def __check_win_left_slash__(chess_board):
-  chess_board_len = len(chess_board)
-  slash_numbers = 2 * chess_board_len - 1
+  return __check_win_lying_flat__(__get_slashes__(chess_board))
+
