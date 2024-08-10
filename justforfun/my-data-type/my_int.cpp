cdc8754fe112330809309a40917b4a81ea2b5567
mao mao <WMC130302@163.com>
2024-08-10 09:36:45 +0000

    fix：改进了pyplot中的my_range

--- justforfun/my-data-type/my_int.cpp
+++ justforfun/my-data-type/my_int.cpp
@@ -1,13 +1,35 @@
 #include <string>
 using std::string;
+#include <vector>
+using std::vector;
+#include <iostream>
+#include <sstream>
+#include <cmath>
 
-struct big_data{
-    string big_data;
-    __uint128_t p1 = int(big_data);
-    __uint128_t p2 = ;
-};
+// string devideBy2(string str){
+//     std::stringstream ss;
+//     for(char i : str){
+//         int num = (i - '0') * 5;
+//         int p2 = num % 10;
+//         int p1 = num - p2;
+//         // p1
+//         ss << num; 
+//     }
+    
+// }
 
+// class int_16{
+// private:
+//     // vector<bool> changeIntoVector(string string){
+
+//     // }
+// public:
+    
+// };
 
 int main(){
-    big_data {, 10000000};
+    string a = "";
+    int s = a.back();
+
+    std::cout << s << std::endl;
 }
---
+++ justforfun/tools/print.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+void print(std::string object){
+    std::cout << object << std::endl;
+}
+
+void print(int object){
+    std::cout << object << std::endl;
+}
+
+void print(double object){
+    std::cout << object << std::endl;
+}
