64af467dfbf9d2c906eba4fe72053abeceee3fb9
mao mao <WMC130302@163.com>
2024-08-29 04:38:27 +0000

    fix:plas.cpp

--- justforfun/RSA/test2.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/multiprecision/cpp_int.hpp>
-using boost::multiprecision::cpp_int;
-
-int main(){
-    cpp_int a("6553765537655376553765537655372222222211133");
-    cpp_int b("100000000000000000000000000000");
-    cpp_int c("33333333333333333333333");
-    boost::multiprecision::powm(b, a);
-}
