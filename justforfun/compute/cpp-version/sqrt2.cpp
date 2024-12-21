308ee3fd46ec7459304c23c27fc10b7963ab3be7
mao mao <WMC130302@163.com>
2024-12-21 09:53:24 +0000

    fix

--- justforfun/compute/cpp-version/sqrt2.cpp
+++ justforfun/compute/cpp-version/sqrt2.cpp
@@ -1,29 +1,10 @@
+#include <chrono>
 #include <iostream>
-#include <cmath>
+#include <boost/multiprecision/cpp_dec_float.hpp>
 
-bool isInLine(double x1) {
-    return (std::pow(0.5 - x1, 2) * 2) <= std::pow(0.5, 2);
-}
-
-double sqrt2_computing(int Precision) {
-    int counter = 0;
-    int totalRolls = 0;
-    int inLineTimes = 0;
-
-    while (counter < Precision) {
-        double x = counter / Precision;
-        if (isInLine(x)) {
-            inLineTimes++;
-        }
-        totalRolls++;
-        counter++;
-    }
-    return static_cast<double>(totalRolls) / inLineTimes;
-}
-
-int main() {
-    int Precision = 1000000; 
-    double sqrt2_approx = sqrt2_computing(Precision);
-    std::cout << "Approximate value of sqrt(2): " << sqrt2_approx << std::endl;
-    return 0;
+int main(){
+    typedef boost::multiprecision::number<boost::multiprecision::cpp_dec_float<50>, boost::multiprecision::et_off> dec_float_50;
+    double a = 1;
+    a /= 9;
+    std::cout << a <<std::endl;
 }
