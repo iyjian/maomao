6dd830a0d2a8a82b3f5d4aa0bccebd57827a8a17
mao mao <WMC130302@163.com>
2025-07-01 11:07:23 +0000

    fix: fix some problems in big_int.cpp

--- justforfun/RSA/big_int/big_int.cpp
+++ justforfun/RSA/big_int/big_int.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <climits>
 
-template <unsigned int bits_of_number>
+template <const unsigned int bits_of_number>
 class big_int{
     private:
         
     public:
-        const unsigned int bits = bits_of_number;
         unsigned int data_limit = bits_of_number / (sizeof(unsigned int));
         unsigned int* data = (unsigned int*)malloc(bits_of_number / 8);
         void set_data(unsigned int num, unsigned int place);
-        bool operator==(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        bool operator!=(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        bool operator<(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        bool operator<=(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        bool operator>(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        bool operator>=(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        big_int<bits_of_number> operator+(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        big_int<bits_of_number> operator-(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        big_int<bits_of_number> operator*(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        big_int<bits_of_number> operator/(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        big_int<bits_of_number> operator%(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        big_int<bits_of_number> operator^(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        big_int<bits_of_number> operator<<(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        big_int<bits_of_number> operator>>(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2);
-        big_int<bits_of_number> power_mod(const big_int<bits_of_number> & base, const big_int<bits_of_number> & exponent, const big_int<bits_of_number> & modulus);
+        bool operator==(const big_int<bits_of_number> & b);
+        bool operator!=(const big_int<bits_of_number> & b);
+        bool operator<(const big_int<bits_of_number> & b);
+        bool operator<=(const big_int<bits_of_number> & b);
+        bool operator>(const big_int<bits_of_number> & b);
+        bool operator>=(const big_int<bits_of_number> & b);
+        big_int<bits_of_number> operator+(const big_int<bits_of_number> & b);
+        // big_int<bits_of_number> operator-(const big_int<bits_of_number> & b);
+        // big_int<bits_of_number> operator*(const big_int<bits_of_number> & b);
+        // big_int<bits_of_number> operator/(const big_int<bits_of_number> & b);
+        // big_int<bits_of_number> operator%(const big_int<bits_of_number> & b);
+        // big_int<bits_of_number> operator^(const big_int<bits_of_number> & b);
+        // big_int<bits_of_number> operator<<(const big_int<bits_of_number> & b);
+        // big_int<bits_of_number> operator>>(const big_int<bits_of_number> & b);
+        // big_int<bits_of_number> power_mod(const big_int<bits_of_number> & base, const big_int<bits_of_number> & exponent, const big_int<bits_of_number> & modulus);
+        void __delete__(){
+            free(data);
+            data = nullptr;
+            data_limit = 0;
+        }
+        big_int(const big_int& other) : data_limit(other.data_limit) {
+            data = (unsigned int*)malloc(data_limit * sizeof(unsigned int));
+            memcpy(data, other.data, data_limit * sizeof(unsigned int));
+        }
+    
+        ~big_int() {
+            free(data);
+            data = nullptr;
+        }
 };
 
-big_int<bits_of_number>::set_data(unsigned int num, unsigned int place){
+template <const unsigned int bits_of_number>
+void big_int<bits_of_number>::set_data(unsigned int num, unsigned int place){
     if(place >= data_limit){
         std::cerr << "Error: Place out of bounds." << std::endl;
         return;
@@ -36,67 +51,83 @@ big_int<bits_of_number>::set_data(unsigned int num, unsigned int place){
     data[place] = num;
 }
 
-bool big_int<bits_of_number>::operator==(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2){
-    if(int_1.data_limit != int_2.data_limit){
+template <const unsigned int bits_of_number>
+bool big_int<bits_of_number>::operator==(const big_int<bits_of_number> & b){
+    if(this->data_limit != b.data_limit){
         std::cerr << "Error: data limits do not match." << std::endl;
         return false;
     }
-    for(int i = int_1.data_limit; i > 0; i++){
-        if(int_1.data[i] != int_2.data[i]) return false;
+    for(int i = 0; i < this->data_limit; i++){
+        if(this->data[i] != b.data[i]) return false;
     }
     return true;
 }
 
-bool big_int<bits_of_number>::operator!=(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2){
-    return !(int_1 == int_2);
+template <const unsigned int bits_of_number>
+bool big_int<bits_of_number>::operator!=(const big_int<bits_of_number> & b){
+    return !(this->data == b.data);
 }
 
-bool big_int<bits_of_number>::operator<(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2){
-    if(int_1.data_limit != int_2.data_limit){
+template <const unsigned int bits_of_number>
+bool big_int<bits_of_number>::operator<(const big_int<bits_of_number> & b){
+    if(this->data_limit != b.data_limit){
         std::cerr << "Error: data limits do not match." << std::endl;
         return false;
     }
-    for(int i = int_1.data_limit; i > 0; i--){
-        if(int_1.data[i] < int_2.data[i]) return true;
-        else if(int_1.data[i] > int_2.data[i]) return false;
+    for(int i = 0; i < this->data_limit; i++){
+        if(this->data[i] < b.data[i]) return true;
+        else if(this->data[i] > b.data[i]) return false;
     }
     return false;
 }
 
-bool big_int<bits_of_number>::operator<=(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2){
-    return !(int_1 < int_2);
+template <const unsigned int bits_of_number>
+bool big_int<bits_of_number>::operator<=(const big_int<bits_of_number> & b){
+    return !(this->data > b.data);
 }
 
-bool big_int<bits_of_number>::operator>(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2){
-    return int_2 < int_1;
+template <const unsigned int bits_of_number>
+bool big_int<bits_of_number>::operator>(const big_int<bits_of_number> & b){
+    return b < this;
 }
 
-bool big_int<bits_of_number>::operator>=(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2){
-    return int_2 <= int_1;
+template <const unsigned int bits_of_number>
+bool big_int<bits_of_number>::operator>=(const big_int<bits_of_number> & b){
+    return b <= this;
 }
 
-big_int<bits_of_number> big_int<bits_of_number>::operator+(const big_int<bits_of_number> & int_1,  big_int<bits_of_number> & int_2){
-    if(int_1.data_limit != int_2.data_limit){
+template <const unsigned int bits_of_number>
+big_int<bits_of_number> big_int<bits_of_number>::operator+(const big_int<bits_of_number> & b){
+    if(this->data_limit != b.data_limit){
         std::cerr << "Error: data limits do not match." << std::endl;
-        return 0;
+        exit(-1);
     }
-    big_int<int_1.bits> result;
+    big_int<bits_of_number> result;
     bool carry = 0;
-    for(int i = 0; i < int_1.data_limit; i++){
-
+    unsigned long temp;
+    for(int i = 0; i < this->data_limit; i++){
+        temp = (unsigned long)this->data[i] + (unsigned long)b.data[i] + carry;
+        if(temp > UINT_MAX){
+            carry = 1;
+            temp -= (UINT_MAX + 1);
+        }
+        else{
+            carry = 0;
+        }
+        result.data[i] = (unsigned int)temp;
     }
-
+    return result;
 }
 
 int main(){
-//     int* location =  (int*)malloc(512);
-//     if (location == NULL){
-//         std::cerr << "Memory allocation failed!" << std::endl;
-//         return 1;
-//     }
-//     location[0] = 100;
-//     location[1] = 200;
-//     std::cout << location << std::endl;
-//     std::cout << location[1] << std::endl;
-//     free(location);
+    big_int<64> a;
+    big_int<64> b;
+    a.set_data(1, 0);
+    b.set_data(2, 0);
+    big_int<64> c = a + b;
+    std::cout << "Result: " << c.data[0] << std::endl; // Should print 3
+    a.__delete__();
+    b.__delete__();
+    c.__delete__();
+    return 0;
 }
